cbad2c274a89e36a67657b59aa71a4648094b5e5
Sabrina Chi <sabrina.chi99@gmail.com>
2016-10-11 04:42:29 +0000

    add fields and const g

--- src/NumberBag.java
+++ src/NumberBag.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
 
 public class NumberBag {
-
+	private ArrayList<Integer> list;
+	private int maxValue;
+	
+	public NumberBag(int max){
+		list = new ArrayList<Integer>();
+		maxValue = max;
+	}
 }
